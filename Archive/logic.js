4d4dd6dfdab051a7ea4d1933519ade0f771fa51f
esdalee <es.da.lee@gmail.com>
2019-05-02 22:27:38 -0400

    functions

---
+++ Archive/logic.js
@@ -0,0 +1,14 @@
+
+// List of words to guess
+var gameWords = ["ironman", "captainamerica", "thor", "thehulk", "blackwidow"];
+
+// Picks random word from gameWords
+function randomWord(array) {
+
+    var randomWord = array[Math.floor(Math.random() * array.length)];
+}
+
+function isCorrectGuess("argument 1", "argument 2") {
+
+
+}
